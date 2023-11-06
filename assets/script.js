deb0def9f04024b58ae64b343575ed9765177da9
marcocolon729 <marcocolon729@gmail.com>
2023-11-06 13:21:56 -0600

    added to script.js

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,53 @@
-function generatePassword() {
-    var length = document.getElementById("length").value;
-    length = parseInt(length);
+document.addEventListener("DOMContentLoaded", function() {
+    var generateBtn = document.getElementById("generateBtn");
 
-    if (isNaN(length) || length < 8 || length > 128) {
-        alert("Please enter a valid password length between 8 and 128 characters.");
-        return;
-    }
+    generateBtn.addEventListener("click", function() {
+        var lengthInput = document.getElementById("length");
+        var passwordField = document.getElementById("password");
 
-    var includeLowercase = confirm("Include lowercase characters?");
-    var includeUppercase = confirm("Include uppercase characters?");
-    var includeNumbers = confirm("Include numeric characters?");
-    var includeSpecialChars = confirm("Include special characters?");
+        var length = parseInt(lengthInput.value);
 
-    if (!includeLowercase && !includeUppercase && !includeNumbers && !includeSpecialChars) {
-        alert("Please select at least one character type for the password.");
-        return;
-    }
+        console.log("Entered password length:", length);
 
-    var charset = "";
-    if (includeLowercase) charset += "abcdefghijklmnopqrstuvwxyz";
-    if (includeUppercase) charset += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    if (includeNumbers) charset += "0123456789";
-    if (includeSpecialChars) charset += "!@#$%^&*()_+?><:{}[]";
+        if (isNaN(length) || length < 8 || length > 128) {
+            console.log("Invalid password length entered.");
+            alert("Please enter a valid password length between 8 and 128 characters.");
+            lengthInput.value = '';
+            passwordField.value = '';
+            return;
+        }
 
-    var password = "";
-    for (var i = 0; i < length; ++i) {
-        var randomChar = Math.floor(Math.random() * charset.length);
-        password += charset[randomChar];
-    }
+        var includeLowercase = confirm("Include lowercase characters?");
+        var includeUppercase = confirm("Include uppercase characters?");
+        var includeNumbers = confirm("Include numeric characters?");
+        var includeSpecialChars = confirm("Include special characters?");
 
-    document.getElementById("password").value = password; // Display the generated password in the input field
-}
+        console.log("Include lowercase:", includeLowercase);
+        console.log("Include uppercase:", includeUppercase);
+        console.log("Include numbers:", includeNumbers);
+        console.log("Include special characters:", includeSpecialChars);
+
+        var charset = '';
+
+        if (includeLowercase) charset += 'abcdefghijklmnopqrstuvwxyz';
+        if (includeUppercase) charset += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        if (includeNumbers) charset += '0123456789';
+        if (includeSpecialChars) charset += '!@#$%^&*()_+?><:{}[]';
+
+        if (charset === '') {
+            alert("Please select at least one character type for the password.");
+            passwordField.value = '';
+            return;
+        }
+
+        var password = '';
+        for (var i = 0; i < length; ++i) {
+            var randomChar = Math.floor(Math.random() * charset.length);
+            password += charset[randomChar];
+        }
+
+        console.log("Generated Password:", password);
+        passwordField.value = password;
+        passwordField.style.backgroundColor = '#c3e6cb';
+    });
+});
